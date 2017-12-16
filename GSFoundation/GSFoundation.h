0f6e0cc8f975d6bf349f0909af5ca8a8be1dcc4c
GloomySunday <121055230@qq.com>
2017-12-16 14:36:34 +0800

    Update: GSFoundation initialized

---
+++ GSFoundation/GSFoundation.h
@@ -0,0 +1,19 @@
+//
+//  GSFoundation.h
+//  GSFoundation
+//
+//  Created by 孟钰丰 on 2017/12/15.
+//  Copyright © 2017年 孟钰丰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GSFoundation.
+FOUNDATION_EXPORT double GSFoundationVersionNumber;
+
+//! Project version string for GSFoundation.
+FOUNDATION_EXPORT const unsigned char GSFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GSFoundation/PublicHeader.h>
+
+
