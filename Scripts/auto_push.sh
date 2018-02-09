a3b11c0b843d5ec2fcc097bcea7d1c52ab193bcc
GloomySunday <121055230@qq.com>
2018-02-09 16:59:54 +0800

    Update: shell content updated

--- Scripts/auto_push.sh
+++ Scripts/auto_push.sh
@@ -18,31 +18,29 @@ else
 
 	# 1.更新 jazzy documents
     echo "Begin Generate jazzy Document...."
-    echo ${Old_Version};
-    # echo "jazzy -m ${Module_Name} -x -workspace,${Module_Name}.xcworkspace,-scheme,${Module_Name} -c -a Gloomy.Sunday -u https://github.com/GloomySunday049 -g https://github.com/orgs/Sky-And-Hammer -o ${Module_Path}/docs --min-acl internal"
 	echo $(cd ${Module_Code_Path}; jazzy -m ${Module_Name} -x -workspace,${Module_Name}.xcworkspace,-scheme,${Module_Name} -c -a Gloomy.Sunday -u https://github.com/GloomySunday049 -g https://github.com/orgs/Sky-And-Hammer -o ${Module_Path}/docs --min-acl internal)
-	# echo "\nGit Commit For Document Updated "
-	# echo $(cd ${Module_Path};git add .; git commit -m "Updated: code documents updated")
+	echo "\nGit Commit For Document Updated "
+	echo $(cd ${Module_Path};git add .; git commit -m "Updated: code documents updated")
 
 	# # # 2.更新 .podspec 版本号
 	echo "\nBegin Update Podspec Version...."
 	echo $(sed -i "" "s/${Old_Version}/ '${Tag_Version}'/g" ${Module_File})
-	# echo "\nGit Commit For Podspec Updated"
-	# echo $(cd ${Module_Path};git add .; git commit -m "Updated: Podspec Version Updated")
+	echo "\nGit Commit For Podspec Updated"
+	echo $(cd ${Module_Path};git add .; git commit -m "Updated: Podspec Version Updated")
 
 
 
 	# 3.GIT PUSH & GIT TAG PUSH
-#    echo "\nGit Push...."
-#    echo $(cd ${Module_Path}; git push)
-#    echo "\nAdd Tag...."
-#    echo $(cd ${Module_Path};git tag -a ${Tag_Version} -m 'Release Version For Cocoapods')
-#    echo "\nGit Push Tag...."
-#    echo $(cd ${Module_Path}; git push origin ${Tag_Version})
-#
-#    # # 4. Thunk to Cocoapods
-#    echo "\n Cocoapods Push..."
-#    echo $(cd ${Module_Path}; pod trunk push --verbose ${Module_File})
+   echo "\nGit Push...."
+   echo $(cd ${Module_Path}; git push)
+   echo "\nAdd Tag...."
+   echo $(cd ${Module_Path};git tag -a ${Tag_Version} -m 'Release Version For Cocoapods')
+   echo "\nGit Push Tag...."
+   echo $(cd ${Module_Path}; git push origin ${Tag_Version})
+
+   # # 4. Thunk to Cocoapods
+   echo "\n Cocoapods Push..."
+   echo $(cd ${Module_Path}; pod trunk push --verbose ${Module_File})
 
 	echo "\nSuccess...."
 fi
